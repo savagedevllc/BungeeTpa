9c0e32ffc94b614df0996b4816bf595ffcb790fa
David Acevedo <19176326+SavageAvocado@users.noreply.github.com>
2024-02-18 20:02:36 -0500

    Java 8.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,8 @@
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
     compileOnly("com.google.code.gson:gson:2.10.1")
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,2 +1,9 @@
 group = "net.savagedev"
 version = "1.0.2"
+
+subprojects {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_9
+        targetCompatibility = JavaVersion.VERSION_1_9
+    }
+}
